8c59cdea75084dda0bd5ece08af48026f918bd55
Dario Piotrowicz <dario@cloudflare.com>
2023-05-14 14:49:38 +0100

    replace application with simple-app-dir-13.3.1

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js Edge API routes: https://nextjs.org/docs/api-routes/edge-api-routes
-
-export const config = {
-  runtime: "edge",
-};
-
-export default function handler() {
-  return new Response("Hello world!");
-}
--- src/pages/routeA/routeA1/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function RouteA1() {
-  return <h1>Route A1</h1>;
-}
