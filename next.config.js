b4b2649b27ace0d9e8914416414ab10dc7006912
Dario Piotrowicz <dario@cloudflare.com>
2023-05-14 14:46:39 +0100

    replace application with simple-src-13.2.4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  appDir: true,
-  experimental: {
-    serverActions: true,
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
